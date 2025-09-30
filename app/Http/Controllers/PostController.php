30c1d56287d04331b2772009f3a027234552d6c7
H1Y128 <one6666hy@gmail.com>
2025-09-30 21:15:06 +0900

    Eloquent ORMを使った課題が完了

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,15 +3,22 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
 {
-    function index () {
+    public function index () {
         $posts = DB::table('posts')->get();
 
         return view('posts/index', compact('posts'));
+    }
+
+    public function show ($id) {
+        $posts = Post::find($id);
+
+        return view('posts.show', compact('posts'));
     }
 }
